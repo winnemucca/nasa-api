be6c74150a00ddba5659442adc0e030efb571f97
winnemucca <stevenkauyedauty@stevens-MacBook-Pro.local>
2020-06-12 23:18:42 -0600

    routing close

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 const routes: Routes = [
   {
+    path: 'welcome',
+    component: WelcomeComponent,
+    data: {
+      title: 'welcome'
+    },
+  },
+  {
+    path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  },
+  {
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then(mod => mod.ProfileModule)
+  },
+  {
+    path: 'nasa-images',
+    loadChildren: () => import('./images/nasa-image/nasa-image.module').then(mod => mod.NasaImageModule)
+  },
+  {
+    path: '**', pathMatch: 'full', component: NotFoundComponent
   }
 ];
 
